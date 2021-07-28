51dbf83877dd5e1ab1a852f2f39d5d5b5aec855d
Chris McColgan <mccolganchrisj@gmail.com>
2021-07-28 15:38:09 -0500

    unit tests

---
+++ Calculator.Tests/InputConverterTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class InputConverterTest
+    {
+        private readonly InputConverter _inputConverter = new InputConverter();
+
+        [TestMethod]
+        public void ConvertValidStringInputIntoDouble()
+        {
+            string inputNumber = "5";
+            double convertedNumber = _inputConverter.ConvertInput(inputNumber);
+            Assert.AreEqual(5, convertedNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailsToConvertInvalidStringInputIntoDouble()
+        {
+            string inputNumber = "*";
+            double convertedNumber = _inputConverter.ConvertInput(inputNumber);
+        }
+    }
+}
